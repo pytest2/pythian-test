b32612c5eab2833abdd33c19eaf8fbe1d928e80a
pytest2 <still+pytest2@pythian.com>
2013-10-21 16:44:40 -0700

    Just added new files for testing

---
+++ clears.sql
@@ -0,0 +1,26 @@
+
+-- clears.sql
+-- init sqlplus environment
+
+set pause off
+set echo off
+set timing off
+set trimspool on
+set feed on term on echo off verify off
+set line 80
+set pages 24 head on
+
+clear col
+clear break
+clear computes
+
+btitle ''
+ttitle ''
+
+btitle off
+ttitle off
+
+set newpage 1
+
+--@@stddate
+
